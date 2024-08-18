f9f0ab7eb1d8763a585c89e2a83df5f9aa607c07
Soham Jaiswal <sosweetham@gmail.com>
2024-08-18 19:25:45 +0530

    feat: on success and on failure

--- src/components/qr.ts
+++ src/components/qr.ts
@@ -2,9 +2,18 @@ import QRCodeStyling, { Options } from "qr-code-styling";
 
 export default class OpenIdQr {
   private _qrOptions: Options;
+  private _onSuccess: () => void;
+  private _onFailure: () => void;
   public qr: QRCodeStyling;
-  constructor(qrOptions: Options, requestUri: string) {
+  constructor(
+    qrOptions: Options,
+    requestUri: string,
+    onSuccess: () => void,
+    onFailure: () => void
+  ) {
     this._qrOptions = qrOptions;
+    this._onSuccess = onSuccess;
+    this._onFailure = onFailure;
     this.qr = new QRCodeStyling({ ...this._qrOptions, data: requestUri });
   }
 }
