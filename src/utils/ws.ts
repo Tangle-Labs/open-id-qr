a16025050f71a561b657bc777ad10ba183c7820e
Coodos <info@coodos.co>
2024-08-18 23:22:44 +0530

    chore: add ws file

---
+++ src/utils/ws.ts
@@ -0,0 +1,26 @@
+export function createWebsocket(uri: string) {
+    let ws: WebSocket;
+    let retries: number;
+    let interval: NodeJS.Timer | null;
+    ws = new WebSocket(uri);
+    ws.onclose = function (e) {
+        console.log(
+            "Socket is closed. Reconnect will be attempted in 1 second.",
+            e.reason
+        );
+        interval = setInterval(function () {
+            console.log("attempting reconnect");
+            ws = new WebSocket(uri);
+            retries++;
+            if (retries < 10) return;
+            if (!interval) return;
+            clearInterval(interval);
+        }, 2000);
+    };
+    ws.onopen = () => {
+        interval = null;
+        if (!interval) return;
+        clearInterval(interval);
+    };
+    return ws;
+}
